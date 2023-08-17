ee411a1ae087e933acfd55dc8600553760408cd8
navneet <mani@indovations.in>
2023-08-17 18:53:48 +0530

    final update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const draggables = document.querySelectorAll('.draggable');
 const containers = document.querySelectorAll('.container');
 
@@ -10,10 +11,29 @@ draggables.forEach(draggable=>{
     })
 })
 
-containers.forEach(container =>{
-    container.addEventListener('dragover',e =>{
-        e.preventDefault()
-        const draggable = document.querySelector('.dragging')
+containers.forEach(container => {
+    container.addEventListener('dragover', e => {
+      e.preventDefault()
+      const afterElement = getDragAfterElement(container, e.clientY)
+      const draggable = document.querySelector('.dragging')
+      if (afterElement == null) {
         container.appendChild(draggable)
+      } else {
+        container.insertBefore(draggable, afterElement)
+      }
     })
-})
+  })
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]
+  
+    return draggableElements.reduce((closest, child) => {
+      const box = child.getBoundingClientRect()
+      const offset = y - box.top - box.height / 2
+      if (offset < 0 && offset > closest.offset) {
+        return { offset: offset, element: child }
+      } else {
+        return closest
+      }
+    }, { offset: Number.NEGATIVE_INFINITY }).element
+  }
